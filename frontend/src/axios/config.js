62a89c3dee8b220a58164da3aaf65dfd2769eac2
Ewerton Ferreira <103693164+ewerton-ferreira@users.noreply.github.com>
2022-12-07 19:11:49 -0300

    Update para teste de deploy

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+require('dotenv').config();
+
+const HOST = process.env.REACT_APP_API_HOST || "localhost";
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
 
 const blogFetch = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: `${PROTOCOL}://${HOST}`,
   headers: {
     "Content-Type": "application/json",
   },
